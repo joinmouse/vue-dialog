a8bc7d91a7611df63f7f988e90e392a93abe4fef
joinmouse <908380434@qq.com>
2017-12-31 19:48:24 +0800

    首页数据懒加载、函数节流的实现

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # vue-youzan
 使用vue重构有赞商城
 
+
+# 重构日志
+1、首页
+- 实现商品推荐列表数据的获取和渲染
+- 触底后更多商品数据的加载(懒加载)
+- 底部导航栏
+- 头部轮播的组件化
+
+
 ## 多页面配置请参考
 https://github.com/joinmouse/vue_multiple_page
+
+
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,26 +1,52 @@
 import Vue from 'vue'
 import axios from 'axios'
-
 //导入css
 import 'css/common.css'
 import './index.css'
-
 //导入mock数据的url
 import url from 'js/api.js'
+//导入mint-ui无线滚动组件UI
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll);
 
 
 let app = new Vue({
   el: '#app',
   data: {
-    lists: null
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false,
+    allLoaded: false
   },
   //created可以拿到Vue的实例(this)
   created() {
-    axios.post(url.hotLists,{
-      pageNum: 1,
-      pageSize: 6
-    }).then(res => {
-      this.lists = res.data.lists
-    })    
+    this.getLists()
+  },
+  methods: {
+    getLists() {
+      //loading:函数节流
+      //allLoaded:判断所有数据是否加载完毕
+      if(this.allLoaded) return
+      this.loading = true
+      axios.post(url.hotLists,{
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then(res => {
+        let currentList = res.data.lists
+        //判断所有数据是否加载完毕
+        if(currentList.length < this.pageSize) {
+          this.allLoaded = true
+        }
+        if(this.lists) {
+          this.lists = this.lists.concat(currentList)
+        }else {
+          //第一次请求数据
+          this.lists = currentList
+        }
+        this.loading = false
+        this.pageNum++
+      })
+    }
   }
 })
