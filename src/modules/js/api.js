feb993dc0bb3ae769c22e646987e5775d128ce0c
joinmouse <908380434@qq.com>
2018-01-01 16:51:07 +0800

    分类页完成数据的获取、渲染

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,11 +1,14 @@
 let url = {
   hotLists: '/index/hotLists',
-  banner: '/index/banner'
+  banner: '/index/banner',
+  topList: '/category/topList',
+  subList: '/category/subList',
+  rank: '/category/rank'
 }
 
 //注意开发环境和上线(真实场景)的切换
 //mock数据的host地址
-let host = 'http://rap.taobao.org/mockjsdata/24297'
+let host = 'http://rap.taobao.org/mockjsdata/24170'
 
 for (let key in url) {
   if (url.hasOwnProperty(key)){
